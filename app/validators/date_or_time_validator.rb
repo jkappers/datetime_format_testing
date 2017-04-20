124ec40ec4dbf13a65c4adef1b9055ec566abffa
Joshua Kappers <josh@jkappers.me>
2017-04-19 21:51:03 -0500

    Throw a TODO to remind me to put the format in the error message

--- app/validators/date_or_time_validator.rb
+++ app/validators/date_or_time_validator.rb
@@ -1,3 +1,5 @@
+# Validates an attribute to ensure the value is a Date, Time, or DateTime
+# TODO: Can we specify the friendly_x_format in the message?
 class DateOrTimeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless [DateTime, Date, Time].any? { |kind| value.is_a? kind }
